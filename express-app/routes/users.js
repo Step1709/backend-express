1a126a245a0f8c3fecbe902b2ea442423e0b7e09
ivolzok <ivolzok@gmail.com>
2026-03-24 17:41:54 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,6 +2,7 @@ let users = [
     {"id": 1, "name": "Козлов Иван"},
     {"id": 2, "name": "Степченко Фёдор"}
 ]
+let next_id = 3
 
 const express = require('express');
 const router = express.Router();
@@ -13,5 +14,13 @@ router.get('/', function(req, res, next) {
   }
   res.send(reqQuest.items);
 });
+
+router.post('/', function(req, res, next) {
+    const newUser = req.body;
+    newUser.id = next_id;
+    next_id++;
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
 
 module.exports = router;
